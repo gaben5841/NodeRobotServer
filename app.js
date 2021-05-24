e164302f48632a3ca4023494a8f4d2135db83540
Gaben <46634297+gaben5841@users.noreply.github.com>
2021-05-24 14:14:57 -0400

    update

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ const app = express()
 const path = require('path')
 const { client } = require('websocket')
 const { createWebSocketStream } = require('ws')
-const { Server } = require('ws');
-const port = process.env.PORT || 3000
+const WebsocketServer = require('ws');
+//const port = process.env.PORT || 3000
+const Port = 3000
 
 var imageData
 var id = 0
@@ -14,7 +15,7 @@ var raspi
 
 //const server = http.createServer(app)
 //const wss = new WebSocket.Server({server})
-/*
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'))
 })
@@ -23,15 +24,15 @@ app.get('/node_modules/base64-js/base64js.min.js', (req, res) => {
   res.sendFile(path.join(__dirname, '/node_modules/base64-js/base64js.min.js'))
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.listen(Port, () => {
+  console.log(`Example app listening at http://localhost:${Port}`)
 })
-*/
+/*
 const server = express()
   .use((req, res) => res.sendFile('index.html', { root: __dirname }))
   .listen(port, () => console.log(`Listening on ${port}`));
-
-const socketServer = new Server({ server });
+*/
+const socketServer = new WebsocketServer.Server({ port: 3030 })
 
 socketServer.on('connection', (socketClient) => {
   console.log('connected')
@@ -51,7 +52,7 @@ socketServer.on('connection', (socketClient) => {
     } else {
       imageData = Buffer.from(data, 'binary')
       socketServer.clients.forEach(function each(client) {
-        if (client !== socketClient && client.readyState === { Server }.OPEN) {
+        if (client !== socketClient && client.readyState === WebsocketServer.OPEN) {
            client.send(imageData);
         } else {
           raspi = socketClient
